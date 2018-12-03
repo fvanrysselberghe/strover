4ffd06e062daa8d474c0659d143dead267d69c77
Filip Van Rysselberghe <filip@byte-atelier.be>
2018-12-03 20:42:17 +0100

    seed user data and roles for development. Required changing the identity

--- src/web/Startup.cs
+++ src/web/Startup.cs
@@ -45,8 +45,10 @@ namespace vlaaienslag
             services.AddDbContext<DataStoreContext>(options => options.UseInMemoryDatabase("myDatabase"));
             
             //services.AddIdentity<IdentityUser, IdentityRole>()
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<DataStoreContext>();
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<DataStoreContext>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
         }
 
         private void ConfigureCommonServices(IServiceCollection services)
@@ -72,7 +74,7 @@ namespace vlaaienslag
             });
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider services)
         {
             if (env.IsDevelopment())
             {
@@ -83,7 +85,6 @@ namespace vlaaienslag
                 app.UseExceptionHandler("/Error");
                 //app.UseHsts();
             }
-
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseAuthentication();
@@ -93,6 +94,68 @@ namespace vlaaienslag
                     name: "default",
                     template: "{controller}/{action=Index}/{id?}");
             });
+
+            CreateRoles(services).Wait();
+//            CreateSuperUser(services).Wait();
+
+            if (env.IsDevelopment())
+            {
+                CreateDefaultUsers(services).Wait();
+            }
+
+        }
+
+        private async Task CreateRoles(IServiceProvider services)
+        {
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            
+            string[] requiredRoles = {"user" , "oc", "administrator"};
+
+            foreach (var requiredRole in requiredRoles)
+            {
+                bool roleExists = await roleManager.RoleExistsAsync(requiredRole);
+                if (!roleExists)
+                    {
+                        var newRole = new IdentityRole(requiredRole);
+                        await roleManager.CreateAsync(newRole);
+                    }
+            }
+        }
+
+        private async Task CreateDefaultUsers(IServiceProvider services)
+        {
+            var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+
+            //add verkoper 1
+            var baseUser = new IdentityUser("verkoper@bc.com");
+            baseUser.Email = "verkoper@bc.com";
+
+            var baseUserResult = await userManager.CreateAsync(baseUser, "Verkoper#123");
+            if (baseUserResult.Succeeded)
+            {
+                var result = await userManager.AddToRoleAsync(baseUser, "user");
+            }
+
+            //add oc-1 = oc
+            var managementUser = new IdentityUser("ouder@anton.com");
+            managementUser.Email = "ouder@anton.com";
+
+            var managementUserResult = await userManager.CreateAsync(managementUser, "Ouder#123");
+            if (managementUserResult.Succeeded)
+            {
+                var result = await userManager.AddToRoleAsync(managementUser, "oc");
+            }
+
+            //add administrator
+            var administrationUser = new IdentityUser("admin@company.com");
+            managementUser.Email = "admin@company.com";
+
+            var administrationUserResult = await userManager.CreateAsync(administrationUser, "Admin#123");
+            if (administrationUserResult.Succeeded)
+            {
+                var result = await userManager.AddToRoleAsync(administrationUser, "administrator");
+            }
+
         }
     }
 }
