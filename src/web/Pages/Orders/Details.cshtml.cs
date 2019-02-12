b47c0e640f89499ec45094f7269b27a6a99f1818
Filip Van Rysselberghe <filip@byte-atelier.be>
2019-02-12 20:05:07 +0100

    Details should include user details

--- src/web/Pages/Orders/Details.cshtml.cs
+++ src/web/Pages/Orders/Details.cshtml.cs
@@ -4,19 +4,24 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Strover.Models;
 using Strover.Application.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Strover.Infrastructure.Data;
 
 namespace Strover.Pages.Orders
 {
     public class DetailsModel : PageModel
     {
         private readonly Strover.Application.Interfaces.IOrderRepository _orders;
+        private readonly UserManager<SalesPerson> _users;
 
-        public DetailsModel(IOrderRepository orders)
+        public DetailsModel(IOrderRepository orders, UserManager<SalesPerson> users)
         {
             _orders = orders;
+            _users = users;
         }
 
         public Order Order { get; set; }
+        public SalesPerson Seller { get; set; }
 
         public async Task<IActionResult> OnGetAsync(string id)
         {
@@ -31,6 +36,8 @@ namespace Strover.Pages.Orders
             {
                 return NotFound();
             }
+
+            Seller = await _users.FindByNameAsync(Order.SellerId);
             return Page();
         }
     }
