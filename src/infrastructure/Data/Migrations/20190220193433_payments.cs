2d67e447574349403a4c43a771a8deb8d1624b8f
Filip Van Rysselberghe <filip@byte-atelier.be>
2019-02-20 20:49:11 +0100

    Add payments migration

---
+++ src/infrastructure/Data/Migrations/20190220193433_payments.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace infrastructure.data.migrations
+{
+    public partial class payments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    ID = table.Column<string>(nullable: false),
+                    State = table.Column<int>(nullable: false),
+                    Amount = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderPayment",
+                columns: table => new
+                {
+                    OrderId = table.Column<string>(nullable: false),
+                    PaymentId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderPayment", x => new { x.OrderId, x.PaymentId });
+                    table.ForeignKey(
+                        name: "FK_OrderPayment_Order_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Order",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderPayment_Payment_PaymentId",
+                        column: x => x.PaymentId,
+                        principalTable: "Payment",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderPayment_PaymentId",
+                table: "OrderPayment",
+                column: "PaymentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderPayment");
+
+            migrationBuilder.DropTable(
+                name: "Payment");
+        }
+    }
+}
