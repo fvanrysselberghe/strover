226dc0a3467902e8c3e2693e3cc5c6602bb75189
fvanrysselberghe <filip@byte-atelier.be>
2020-02-11 22:18:58 +0100

    Add migration with payment model extensions

---
+++ src/infrastructure/Data/Migrations/20200211203848_PaymentExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class PaymentExtension : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExecutionDate",
+                table: "Payment",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Method",
+                table: "Payment",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Reference",
+                table: "Payment",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExecutionDate",
+                table: "Payment");
+
+            migrationBuilder.DropColumn(
+                name: "Method",
+                table: "Payment");
+
+            migrationBuilder.DropColumn(
+                name: "Reference",
+                table: "Payment");
+        }
+    }
+}
