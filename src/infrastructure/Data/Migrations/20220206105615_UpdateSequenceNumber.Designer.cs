161009c48eeca8d2b6f586db100cb21e604256ef
fvanrysselberghe <filip@byte-atelier.be>
2022-02-06 12:23:00 +0100

    migrate sequencenumber

--- src/infrastructure/Data/Migrations/20220206105615_UpdateSequenceNumber.Designer.cs
+++ src/infrastructure/Data/Migrations/20220206105615_UpdateSequenceNumber.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Strover.Models;
 
-namespace infrastructure.Data.Migrations
+namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(DataStoreContext))]
-    partial class DataStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20220206105615_UpdateSequenceNumber")]
+    partial class UpdateSequenceNumber
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/infrastructure/Data/Migrations/20220206105615_UpdateSequenceNumber.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class UpdateSequenceNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SequenceNumber",
+                table: "Product",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "SequenceNumber",
+                table: "Product",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
