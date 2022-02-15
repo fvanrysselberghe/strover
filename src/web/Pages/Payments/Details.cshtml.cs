7161e4ba448783689446c54038f116ed2d6398d0
fvanrysselberghe <filip@byte-atelier.be>
2022-02-15 20:59:12 +0100

    Make details actionable

--- src/web/Pages/Payments/Details.cshtml.cs
+++ src/web/Pages/Payments/Details.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 using Strover.Models;
 
 namespace Strover.Pages
@@ -11,13 +12,18 @@ namespace Strover.Pages
     public class DetailsModel : PageModel
     {
         private DataStoreContext _context;
+        private ShopOptions _config;
 
-        public DetailsModel(DataStoreContext context)
+        public DetailsModel(DataStoreContext context, IOptions<ShopOptions> shopConfiguration)
         {
             _context = context;
+
+            _config = shopConfiguration.Value;
         }
 
         public Payment Payment { get; set; }
+
+        public String Beneficiary => _config.AccountNumber;
 
         public IActionResult OnGet(string id)
         {
